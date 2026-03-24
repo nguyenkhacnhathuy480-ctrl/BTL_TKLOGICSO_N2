e49970a413066c53244239fa81447be3988e7257
Nguyễn Khắc Nhật Huy <nguyenkhacnhathuy480@gmail.com>
2026-03-24 22:58:28 +0700

    Update mod7_counter.v

--- mod7_counter.v
+++ mod7_counter.v
@@ -2,17 +2,17 @@ module mod7_counter(
     input clk,
     input rst,
     input en,
-    output reg [2:0] count
+    output reg [3:0] count // SỬA: Tăng lên 4 bit (để chứa được số 8)
 );
-    // Không cần cout vì đây là cấp cao nhất của RTC này
+    // Không cần carry_out vì đây là cấp cao nhất của RTC này
     always @(posedge clk or posedge rst) begin
         if (rst) 
-            count <= 3'd1; // Reset về 1 (Thứ Hai)
+            count <= 4'd2; // Reset về 2 (Thứ Hai)
         else if (en) begin
-            if (count == 3'd7) 
-                count <= 3'd1; // Đạt Chủ Nhật thì quay về Thứ Hai
+            if (count == 4'd8) 
+                count <= 4'd2; // Đạt Chủ Nhật (8) thì quay về Thứ Hai (2)
             else 
-                count <= count + 1;
+                count <= count + 4'd1;
         end
     end
 endmodule
